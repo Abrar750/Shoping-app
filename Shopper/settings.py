514d6a3e71d7bcb4ccb687191c435ca0e55e1a31
Abrar <abrar123@gmail.com>
2022-04-11 18:55:25 -0700

    First commit

--- Shopper/settings.py
+++ Shopper/settings.py
@@ -129,11 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -149,4 +146,3 @@ EMAIL_USE_TLS = True
 
 MEDIA_URL="images/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
