12ddd6d6fa4fdf97f6dd2c134aa517777cdd786f
Abrar <abrar123@gmail.com>
2022-04-11 20:20:21 -0700

    Sqlite

--- Shopper/settings.py
+++ Shopper/settings.py
@@ -84,12 +84,9 @@ WSGI_APPLICATION = 'Shopper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projects',
-        'USER':'root',
-        'PASSWORD':'',
-        'PORT':'3306',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
